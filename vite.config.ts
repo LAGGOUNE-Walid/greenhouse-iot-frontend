5bc7696efd4d47a206e7f9268e21422ab2923b7b
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2025-03-15 18:45:29 +0100

    modifed vite to allow any host

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true,
+  },
   plugins: [
     vue(),
     vueJsx(),
