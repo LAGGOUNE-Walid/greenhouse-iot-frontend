532d3d9de614cf0cdeef28f192cc6cbff3094639
LAGGOUNE-Walid <walidlaggoune159@gmail.com>
2025-04-30 10:54:16 +0100

    Showing each moisture node data by one

--- src/components/Datasets/DataStatsOne.vue
+++ src/components/Datasets/DataStatsOne.vue
@@ -16,16 +16,7 @@ function fetchCardsData() {
         eventSource.onmessage = function (event) {
             const apiData = JSON.parse(event.data).data
             lastUpdated.value = new Date().toLocaleTimeString();
-
             cardItems.value = [
-                {
-                    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
-              <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267"/>
-            </svg>`,
-                    title: 'Soil Moisture',
-                    total: `${apiData.soil_moisture.value} %`,
-                    created_at: `${apiData.soil_moisture.created_at}`
-                },
                 {
                     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-sun" viewBox="0 0 16 16">
               <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V2.5a.5.5 0 0 1 1 0v8.585A1.5 1.5 0 0 1 5 12.5"/>
@@ -33,6 +24,7 @@ function fetchCardsData() {
             </svg>`,
                     title: 'Temperature',
                     total: `${apiData.temperature.value} °C`,
+                    node_id: `${apiData.temperature.node_id}`,
                     created_at: `${apiData.temperature.created_at}`
                 },
                 {
@@ -42,6 +34,7 @@ function fetchCardsData() {
             </svg>`,
                     title: 'Humidity',
                     total: `${apiData.humidity.value} %`,
+                    node_id: `${apiData.temperature.node_id}`,
                     created_at: `${apiData.humidity.created_at}`
                 },
                 {
@@ -51,10 +44,25 @@ function fetchCardsData() {
             </svg>`,
                     title: 'Pressure',
                     total: `${apiData.pressure.value} hPa`,
+                    node_id: `${apiData.temperature.node_id}`,
                     created_at: `${apiData.pressure.created_at}`
                 }
             ];
+            apiData.soil_moisture.forEach((sm, index, array) => {
+                cardItems.value.push(
+                    {
+                        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
+              <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267"/>
+            </svg>`,
+                        title: 'Soil Moisture',
+                        total: `${sm.value} %`,
+                        node_id: `${sm.node_id}`,
+                        created_at: `${sm.created_at}`
+                    },
+                )
+            })
         }
+
         eventSource.onerror = (err) => {
             console.error("EventSource failed:", err);
         };
@@ -69,9 +77,9 @@ onMounted(() => {
 });
 
 onUnmounted(() => {
-  if (eventSource) {
-    eventSource.close();
-  }
+    if (eventSource) {
+        eventSource.close();
+    }
 });
 </script>
 
@@ -86,6 +94,8 @@ onUnmounted(() => {
             <div>
                 <h4 class="text-title-md font-bold text-black dark:text-white">{{ item.total }}</h4>
                 <span class="text-sm font-medium">{{ item.title }}</span>
+                <br/>
+                <span class="text-sm font-medium">Node id #{{ item.node_id }}</span>
                 <br />
                 <span class="text-sm font-medium">At {{ item.created_at }}</span>
             </div>
